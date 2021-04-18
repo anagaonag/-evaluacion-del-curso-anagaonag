5f3bd93ea4b1d85624c0e000d4359d6cc4bd9173
Ana María Gaona Gómez <62185751+anagaonag@users.noreply.github.com>
2021-04-18 10:16:28 -0500

    Update question.py

--- 02-python/q05/question.py
+++ 02-python/q05/question.py
@@ -16,4 +16,25 @@
 ##
 ##  >>> Escriba su codigo a partir de este punto <<<
 ##
+import csv
+text=[]
+with open('data.csv', 'rt') as f:
+    text += f.readlines()
+text = [line.replace("\n","").split("\t") for line in text]
+column1 = [row[0] for row in text]
+column2 = [row[1] for row in text]
+keys = set(column1)
+tuplas = []
+for key in keys:
+    min = int(column2[0])
+    max = int(column2[0])
+    for i in range(len(column1)):
+        if column1[i] == key:
+            if int(column2[i]) < min:
+                min = int(column2[i])
+            if int(column2[i]) > max:
+                max = int(column2[i])
+    tuplas.append((key,max,min))
+for tupla in sorted(tuplas):
+    print(str(tupla[0])+","+str(tupla[1])+","+str(tupla[2])) 
 

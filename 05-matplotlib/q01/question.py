bd4db019ae1d77a8604243282ac9d4f5944a6f40
Ana María Gaona Gómez <62185751+anagaonag@users.noreply.github.com>
2021-04-18 10:17:39 -0500

    Update question.py

--- 05-matplotlib/q01/question.py
+++ 05-matplotlib/q01/question.py
@@ -12,3 +12,20 @@
 ##
 ##  >>> Escriba su codigo a partir de este punto <<<
 ##
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.ticker as tic
+import pandas as pd
+import random
+data = pd.read_csv('data.csv', sep=',', index_col='Region')
+fig, axs = plt.subplots(nrows=1, ncols=6,  figsize=(13,6), dpi =72, sharey=True)
+fig.subplots_adjust(hspace=0.1, wspace=0.05)
+for x, y  in list(enumerate(data.index)):
+    axs[x].scatter(range(3), data.loc[y], color=['tab:orange', 'tab:blue', 'tab:green'], facecolors='none', linewidth = 3, s=200)
+    axs[x].set_title(y)
+    axs[x].set_xticks(range(3))
+    axs[x].set_xticklabels(data.columns, rotation='vertical')
+    axs[x].margins(x=0.15, y=0.05)
+axs[0].set_ylabel('Poblacion')
+fig.tight_layout()
+plt.savefig('generada.png', dpi=72)

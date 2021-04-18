83fa5d955cc3fc98e71adbb0a089f408bafa1359
Ana María Gaona Gómez <62185751+anagaonag@users.noreply.github.com>
2021-04-18 10:16:43 -0500

    Update question.py

--- 02-python/q11/question.py
+++ 02-python/q11/question.py
@@ -18,4 +18,21 @@
 ##
 ##  >>> Escriba su codigo a partir de este punto <<<
 ##
-
+import csv
+text = []
+with open('data.csv', 'rt') as f:
+    text += f.readlines()
+text = [line.replace("\n","").split("\t") for line in text]
+column2 = [row[1] for row in text]
+column4 = [row[3] for row in text]
+lista = [[element,column2[i]] for i in range(len(column4)) for element in column4[i].split(",")]
+keys = set(row[0] for row in lista)
+tuplas = []
+for key in keys:
+    sum = 0
+    for i in range(len(lista)):
+        if lista[i][0] == key:
+            sum += int(lista[i][1])
+    tuplas.append([key,sum])
+for tupla in sorted(tuplas):
+    print(str(tupla[0])+","+str(tupla[1]))

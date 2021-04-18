8a55f68efe11c0474a1b40a919abd361b8427eab
Ana María Gaona Gómez <62185751+anagaonag@users.noreply.github.com>
2021-04-18 10:16:45 -0500

    Update question.py

--- 02-python/q12/question.py
+++ 02-python/q12/question.py
@@ -18,6 +18,18 @@
 ##
 ##  >>> Escriba su codigo a partir de este punto <<<
 ##
-
+import csv
+text=[]
+with open('data.csv', 'rt') as f:
+    text += f.readlines()
+text = [line.replace("\n","").split("\t") for line in text]
+column1 = [row[0] for row in text]
+column5 = [row[4] for row in text]
+for i in range(len(column1)):
+    sum = 0
+    valores = [element.split(":")[1] for element in column5[i].split(",")]
+    for val in valores:
+        sum += int(val)
+    print(column1[i]+","+str(sum))
 
 

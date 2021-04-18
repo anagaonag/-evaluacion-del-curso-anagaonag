40d49fface51096c66a2a7dcaebdcab9660118db
Ana María Gaona Gómez <62185751+anagaonag@users.noreply.github.com>
2021-04-18 10:17:43 -0500

    Update question.py

--- 05-matplotlib/q02/question.py
+++ 05-matplotlib/q02/question.py
@@ -12,3 +12,26 @@
 ##
 ##  >>> Escriba su codigo a partir de este punto <<<
 ##
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import pandas as pd
+import os
+df=pd.read_csv(open('data.csv'), delimiter=',')
+copia = df.copy()
+(df.loc[df['Region'] == "Asia", "Region"]) = "Asia"
+aux = (df.groupby('Region')['Poblacion 0-14','Poblacion 15-64','Poblacion 65+'].sum()).sort_values(by='Region')
+auxT = aux.T
+fig,axs = plt.subplots(1, 6, sharex='col', sharey='row', figsize=(13, 6), dpi=72)
+plt.subplots_adjust(wspace = 0, hspace=0.1)
+plt.setp(axs[0], ylabel='Poblacion')
+plt.style.use('default')
+colors = ['tab:orange', 'tab:blue', 'tab:green']
+for i, colname in enumerate(auxT.columns):
+    axs[i].bar(list(auxT.index), auxT[colname], color=colors)
+    axs[i].set_title(colname)
+    plt.style.use('default')
+    for tick in axs[i].get_xticklabels():
+        tick.set_rotation(90)
+plt.tight_layout()
+plt.savefig('generada.png')
